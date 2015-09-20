f99978a2b40579d09fff1b6163bc08d8a097d7f0
Gary Borton <gdborton@gmail.com>
2015-09-19 20:29:34 -0400

    Update extension work logic.

--- structures.js
+++ structures.js
@@ -1,10 +1,10 @@
 var structureTypes = {};
 structureTypes[STRUCTURE_EXTENSION] = function() {
-  this.room.createConstructionSite(this.pos.x - 1, this.pos.y - 1);
+  this.room.createConstructionSite(this.pos.x - 1, this.pos.y - 1, STRUCTURE_EXTENSION);
 };
 
 Structure.prototype.work = function() {
   if (structureTypes[this.structureType]) {
-    structureTypes[this.structureType]();
+    structureTypes[this.structureType].call(this);
   }
 };
