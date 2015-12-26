74099a0caf6e2497e557978652b8be589d6eca4e
Gary Borton <gdborton@gmail.com>
2015-12-26 02:37:38 -0500

    Only process extensions once every 10 ticks... this is a shortcut to save CPU.

--- structures.js
+++ structures.js
@@ -1,10 +1,12 @@
 var structureTypes = {};
 structureTypes[STRUCTURE_EXTENSION] = function() {
-  if (this.room.canBuildExtension()) {
-    this.room.createConstructionSite(this.pos.x - 1, this.pos.y - 1, STRUCTURE_EXTENSION);
-  }
-  if (this.room.canBuildExtension()) {
-    this.room.createConstructionSite(this.pos.x - 1, this.pos.y + 1, STRUCTURE_EXTENSION);
+  if (Game.time % 10 === 0) {
+    if (this.room.canBuildExtension()) {
+      this.room.createConstructionSite(this.pos.x - 1, this.pos.y - 1, STRUCTURE_EXTENSION);
+    }
+    if (this.room.canBuildExtension()) {
+      this.room.createConstructionSite(this.pos.x - 1, this.pos.y + 1, STRUCTURE_EXTENSION);
+    }
   }
 };
 
