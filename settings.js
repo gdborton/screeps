07c33cfb1a2b70dadcd14a434e268d10fb9670cd
Gary Borton <gdborton@gmail.com>
2015-12-28 17:34:55 -0500

    Add settings for controller level 7 and 8.

--- settings.js
+++ settings.js
@@ -41,6 +41,22 @@ module.exports = {
       walls: true,
       roads: true,
       storage: 1
+    },
+    7: {
+      spawns: 1,
+      extensions: 50,
+      ramparts: true,
+      walls: true,
+      roads: true,
+      storage: 1
+    },
+    8: {
+      spawns: 1,
+      extensions: 60,
+      ramparts: true,
+      walls: true,
+      roads: true,
+      storage: 1
     }
   }
 };
