a9c83afb408fb8fff8f5b6d39657b385610e4181
Gary Borton <gdborton@gmail.com>
2015-12-30 04:19:15 -0500

    Add some memory for tracking used and wasted cpu.

--- main.js
+++ main.js
@@ -8,6 +8,16 @@ var profiler = require('profiler');
 
 profiler.enable();
 module.exports.loop = function() {
+  var wastedCPU = Game.getUsedCpu();
+  if (!Memory.requireProfile) {
+    Memory.requireProfile = {
+      requireCPU: 0,
+      ticks: 0,
+      runningOverhead: 0,
+      usedCPU: 0,
+      avgUsedCPU: 0
+    }
+  }
   profiler.wrap(function() {
     var index = 0;
     for (var roomName in Game.rooms) {
@@ -17,4 +27,9 @@ module.exports.loop = function() {
       }
     }
   });
+  Memory.requireProfile.requireCPU = Memory.requireProfile.requireCPU + wastedCPU;
+  Memory.requireProfile.ticks++;
+  Memory.requireProfile.runningOverhead = Memory.requireProfile.requireCPU / Memory.requireProfile.ticks;
+  Memory.requireProfile.usedCPU = Memory.requireProfile.usedCPU + Game.getUsedCpu() - wastedCPU;
+  Memory.requireProfile.avgUsedCPU = Memory.requireProfile.usedCPU / Memory.requireProfile.ticks;
 }
