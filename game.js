dde8b888dac9c8ba0b5fe8c22b6dd91156998fb6
Gary Borton <gdborton@gmail.com>
2016-01-18 03:33:38 -0500

    Make Game object setup a function.

--- game.js
+++ game.js
@@ -1,13 +1,17 @@
-Game.clearScoutFlags = function() {
-  Game.getScoutFlags().forEach((flag) => {
-    flag.remove();
-  });
-};
+module.exports = {
+  setup: function() {
+    Game.clearScoutFlags = function() {
+      Game.getScoutFlags().forEach((flag) => {
+        flag.remove();
+      });
+    };
 
-Game.getScoutFlags = function() {
-  return Object.keys(Game.flags).filter(function(flagName) {
-    return flagName.toLowerCase().indexOf('scout') !== -1;
-  }).map(function(flagName) {
-    return Game.flags[flagName];
-  });
-};
+    Game.getScoutFlags = function() {
+      return Object.keys(Game.flags).filter(function(flagName) {
+        return flagName.toLowerCase().indexOf('scout') !== -1;
+      }).map(function(flagName) {
+        return Game.flags[flagName];
+      });
+    };
+  }
+}
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require('perf')();
-require('game');
+var game = require('game');
 require('creep');
 require('source');
 require('spawns');
@@ -9,19 +9,14 @@ var profiler = require('profiler');
 profiler.enable();
 
 module.exports.loop = function() {
-  // var start = Game.getUsedCpu();
   if (Room.prototype.work && Game.cpuLimit > 100) {
-    // var workStart = 0;
-    // var workEnd = 0;
     profiler.wrap(function () {
-      //workStart = Game.getUsedCpu();
+      game.setup();
       Object.keys(Game.rooms).forEach((roomName, index) => {
         if (index === 1 || Game.cpuLimit > 50) {
           Game.rooms[roomName].work();
         }
       });
-      //workEnd = Game.getUsedCpu();
     });
-    //console.log('require time', start, 'work', workEnd - workStart, 'wrapped', Game.getUsedCpu() - start, 'total', Game.getUsedCpu());
   }
 }
