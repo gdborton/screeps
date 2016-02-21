2dffd2a35fef2aa6c98002b65d1e630aa5ac6136
Gary Borton <gdborton@gmail.com>
2016-02-21 05:06:15 -0800

    Lint RoomPosition.

--- src/room-position.js
+++ src/room-position.js
@@ -1,15 +1,17 @@
-RoomPosition.prototype.identifier = function() {
-  return this.roomName + 'x' + this.x + 'y' + this.y;
+RoomPosition.prototype.identifier = function identifier() {
+  return `${this.roomName}x${this.x}y${this.y}`;
 };
 
-RoomPosition.prototype.freeEdges = function() {
+RoomPosition.prototype.freeEdges = function freeEdges() {
   if (!(Memory.freeEdges && Memory.freeEdges[this.identifier()])) {
-    var openSpots = 0;
-    var surroundings = Game.rooms[this.roomName].lookAtArea(this.y - 1, this.x - 1, this.y + 1, this.x + 1);
-    Object.keys(surroundings).forEach(function(x) {
-      Object.keys(surroundings[x]).forEach(function(y) {
-        openSpots = openSpots + surroundings[x][y].filter(function(object) {
-          return object.type === 'terrain' && (object.terrain === 'swamp' || object.terrain === 'plain');
+    let openSpots = 0;
+    const room = Game.rooms[this.roomName];
+    const surroundings = room.lookAtArea(this.y - 1, this.x - 1, this.y + 1, this.x + 1);
+    Object.keys(surroundings).forEach(x => {
+      Object.keys(surroundings[x]).forEach(y => {
+        openSpots = openSpots + surroundings[x][y].filter(object => {
+          const isTerrain = object.type === 'terrain';
+          return isTerrain && (object.terrain === 'swamp' || object.terrain === 'plain');
         }).length;
       });
     });
